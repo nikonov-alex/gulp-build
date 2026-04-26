78a6657b663c2de35e315d2ae55d30e905d6142d
Nikonov Aleksander <nikonov.test.mail@gmail.com>
2026-04-27 01:48:12 +0300

    add esbuild options

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+import esbuildBundler from "./lib/gulp-esbuild";
 type Stream = NodeJS.ReadWriteStream;
 declare const map: (plugin: Stream) => (instream: Stream) => NodeJS.ReadWriteStream;
 declare const civet: (stream: Stream) => NodeJS.ReadWriteStream & {
@@ -6,8 +7,9 @@ declare const civet: (stream: Stream) => NodeJS.ReadWriteStream & {
     };
 };
 declare const babel: (stream: Stream) => NodeJS.ReadWriteStream;
-declare const esbuild: (entry: string, outfile: string) => (stream: Stream) => NodeJS.ReadWriteStream;
-declare const makeTask: ({ input, output }: {
+type BundlerOptions = Omit<esbuildBundler.Options, "entry">;
+declare const esbuild: (entry: string, outfile: string, options?: BundlerOptions) => (stream: Stream) => NodeJS.ReadWriteStream;
+declare const makeTask: ({ input, output, ...options }: {
     input: {
         files: string | string[];
         entry: string;
@@ -16,6 +18,7 @@ declare const makeTask: ({ input, output }: {
         dir: string;
         filename: string;
     };
+    esbuild?: BundlerOptions;
 }) => () => NodeJS.ReadWriteStream;
 export { map, civet, babel, esbuild };
 export default makeTask;

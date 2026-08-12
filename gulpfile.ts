c0b2d52024777819121fd7eb94519e70adff3bc1
Nikonov Aleksander <nikonov.test.mail@gmail.com>
2026-08-12 12:10:58 +0300

    imports - to requires

--- gulpfile.ts
+++ gulpfile.ts
@@ -2,6 +2,7 @@ import gulp from "gulp";
 import civet from "gulp-civet";
 import ts from "gulp-typescript";
 const typescript = ts.createProject( "tsconfig.json" );
+import babel from "gulp-babel";
 
 
 const build = () =>
@@ -11,6 +12,14 @@ const build = () =>
             js: false
         } ) )
         .pipe( typescript() )
+        .pipe( babel( {
+            presets: [
+                 //@ts-ignore
+                ["@babel/preset-env", {
+                    modules: "commonjs"
+                }]
+            ]
+        } ) )
         .pipe( gulp.dest( "." ) );
 
 
--- index.js
+++ index.js
@@ -1,69 +1,87 @@
-import gulp from "gulp";
-import path from 'node:path';
-import civet from "gulp-civet";
-import ts from "gulp-typescript";
-const typescript = ts.createProject("tsconfig.json");
-import rename from "gulp-rename";
-import esbuild from "./lib/gulp-esbuild";
-import dartSass from "sass";
-import gulpSass from "gulp-sass";
-const sass = gulpSass(dartSass);
-import merge from "merge-stream";
-import babel from "gulp-babel";
-import minify from "gulp-terser";
-import postcss from "gulp-postcss";
-const map = (plugin) => {
-    return (stream) => {
-        return stream.pipe(plugin);
-    };
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+require("core-js/modules/es.iterator.constructor.js");
+require("core-js/modules/es.iterator.map.js");
+var _gulp = _interopRequireDefault(require("gulp"));
+var _nodePath = _interopRequireDefault(require("node:path"));
+var _gulpCivet = _interopRequireDefault(require("gulp-civet"));
+var _gulpTypescript = _interopRequireDefault(require("gulp-typescript"));
+var _gulpRename = _interopRequireDefault(require("gulp-rename"));
+var _gulpEsbuild = _interopRequireDefault(require("./lib/gulp-esbuild"));
+var _sass = _interopRequireDefault(require("sass"));
+var _gulpSass = _interopRequireDefault(require("gulp-sass"));
+var _mergeStream = _interopRequireDefault(require("merge-stream"));
+var _gulpBabel = _interopRequireDefault(require("gulp-babel"));
+var _gulpTerser = _interopRequireDefault(require("gulp-terser"));
+var _gulpPostcss = _interopRequireDefault(require("gulp-postcss"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+const typescript = _gulpTypescript.default.createProject("tsconfig.json");
+const sass = (0, _gulpSass.default)(_sass.default);
+const map = plugin => {
+  return stream => {
+    return stream.pipe(plugin);
+  };
 };
-const id = (value) => value;
+const id = value => value;
 const skip = id;
-const makeTask = ({ input, output, development, ...options }) => {
-    const files = (relative) => {
-        return path.join(input.sources, relative);
-    };
-    const ext = (...extensions) => {
-        const globs = extensions.map((ext) => files(`/**/*${ext}`));
-        return gulp.src(globs);
-    };
-    const maybe = (plugin) => {
-        if (development) {
-            return skip;
-        }
-        else
-            return map(plugin);
-    };
-    let ref;
-    {
-        const civets = map(civet({
-            extension: '.tsx',
-            js: false
-        }))(ext(".civet"));
-        ref = map(rename({ extname: ".civet" }))(map(typescript())(merge(civets, ext(".d.ts"))));
-    }
-    ;
-    const scripts = ref;
-    let ref1;
-    {
-        const sasses = map(rename({ extname: ".sass" }))(map(sass().on('error', sass.logError))(ext(".sass")));
-        const scsses = map(rename({ extname: ".scss" }))(map(sass().on('error', sass.logError))(ext(".scss")));
-        ref1 = maybe(postcss())(merge(sasses, scsses));
-    }
-    ;
-    const styles = ref1;
-    const bundler = esbuild({
-        entry: files(input.entry),
-        bundle: true,
-        loader: {
-            ".civet": "js",
-            '.css': 'text',
-            '.scss': 'text',
-            '.sass': 'text'
-        },
-        resolveExtensions: [".js", ".jsx", ".mjs", ".civet"],
-        ...options.esbuild
-    });
-    return map(gulp.dest(output.dir))(map(rename(output.filename))(maybe(minify())(maybe(babel())(map(bundler)(merge(scripts, styles))))));
+const makeTask = ({
+  input,
+  output,
+  development,
+  ...options
+}) => {
+  const files = relative => {
+    return _nodePath.default.join(input.sources, relative);
+  };
+  const ext = (...extensions) => {
+    const globs = extensions.map(ext => files(`/**/*${ext}`));
+    return _gulp.default.src(globs);
+  };
+  const maybe = plugin => {
+    if (development) {
+      return skip;
+    } else return map(plugin);
+  };
+  let ref;
+  {
+    const civets = map((0, _gulpCivet.default)({
+      extension: '.tsx',
+      js: false
+    }))(ext(".civet"));
+    ref = map((0, _gulpRename.default)({
+      extname: ".civet"
+    }))(map(typescript())((0, _mergeStream.default)(civets, ext(".d.ts"))));
+  }
+  ;
+  const scripts = ref;
+  let ref1;
+  {
+    const sasses = map((0, _gulpRename.default)({
+      extname: ".sass"
+    }))(map(sass().on('error', sass.logError))(ext(".sass")));
+    const scsses = map((0, _gulpRename.default)({
+      extname: ".scss"
+    }))(map(sass().on('error', sass.logError))(ext(".scss")));
+    ref1 = maybe((0, _gulpPostcss.default)())((0, _mergeStream.default)(sasses, scsses));
+  }
+  ;
+  const styles = ref1;
+  const bundler = (0, _gulpEsbuild.default)({
+    entry: files(input.entry),
+    bundle: true,
+    loader: {
+      ".civet": "js",
+      '.css': 'text',
+      '.scss': 'text',
+      '.sass': 'text'
+    },
+    resolveExtensions: [".js", ".jsx", ".mjs", ".civet"],
+    ...options.esbuild
+  });
+  return map(_gulp.default.dest(output.dir))(map((0, _gulpRename.default)(output.filename))(maybe((0, _gulpTerser.default)())(maybe((0, _gulpBabel.default)())(map(bundler)((0, _mergeStream.default)(scripts, styles))))));
 };
-export default makeTask;
+var _default = exports.default = makeTask;
